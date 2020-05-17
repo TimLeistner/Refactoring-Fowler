c822fee1cfbc9495ebdefde57136e955638e3c20
Tim Leistner <tim.leistner99@gmail.com>
2020-05-17 20:35:06 +0200

    Code smell: Feature Envy
    Method: Replace Temp with Query

--- src/refactoring_exercise/Customer.java
+++ src/refactoring_exercise/Customer.java
@@ -5,18 +5,22 @@ import java.util.*;
 
 class Customer {
     private String name;
+
     private Vector rentals = new Vector();
+
     public Customer (String newname){
         name = newname;
     };
+
     public void addRental(Rental arg) {
         rentals.addElement(arg);
     };
+
     public String getName (){
         return name;
     };
+
     public String statement() {
-        double totalAmount = 0;
         int frequentRenterPoints = 0;
         Enumeration enum_rentals = rentals.elements();	    
         String result = "Rental Record for " + this.getName() + "\n";
@@ -24,15 +28,31 @@ class Customer {
 
         while (enum_rentals.hasMoreElements()) {
             Rental each = (Rental) enum_rentals.nextElement();
-            //determine amounts for each line
-            frequentRenterPoints += each.getFrequentRenterPoints();
-            //show figures for this rental
             result += "\t" + each.getMovie().getTitle()+ "\t" + "\t" + each.getDaysRented() + "\t" + String.valueOf(each.getCharge()) + "\n";
-            totalAmount += each.getCharge();
         }
         //add footer lines
-        result += "Amount owed is " + String.valueOf(totalAmount) + "\n";
-        result += "You earned " + String.valueOf(frequentRenterPoints) + " frequent renter points";
+        result += "Amount owed is " + String.valueOf(getTotalCharge()) + "\n";
+        result += "You earned " + String.valueOf(getTotalFrequentRenterPoints()) + " frequent renter points";
+        return result;
+    }
+
+    double getTotalCharge(){
+        double result = 0;
+        Enumeration enum_rentals = rentals.elements();
+        while (enum_rentals.hasMoreElements()) {
+            Rental each = (Rental) enum_rentals.nextElement();
+            result += each.getCharge();
+        }
+        return result;
+    }
+
+    int getTotalFrequentRenterPoints(){
+        int result = 0;
+        Enumeration enum_rentals = rentals.elements();
+        while (enum_rentals.hasMoreElements()) {
+            Rental each = (Rental) enum_rentals.nextElement();
+            result += each.getFrequentRenterPoints();
+        }
         return result;
     }
 
